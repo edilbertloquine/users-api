726ad23cb8ca15fa7c31b2ccf002b303200b8157
Edilbert Loquine <edilbert@whitewidget.com>
2020-05-26 22:55:18 +0800

    feat(project): Added error handlers and struct

--- controllers/users/users_controller.go
+++ controllers/users/users_controller.go
@@ -3,11 +3,41 @@ package users
 import (
 	"net/http"
 
+	"github.com/edilbertloquine/go-microservices/users-api/domain/users"
+	"github.com/edilbertloquine/go-microservices/users-api/services"
+	"github.com/edilbertloquine/go-microservices/users-api/utils/errors"
+
 	"github.com/gin-gonic/gin"
 )
 
 func CreateUser(c *gin.Context) {
-	c.String(http.StatusNotImplemented, "Implement this")
+	var user users.User
+
+	// bytes, err := ioutil.ReadAll(c.Request.Body)
+	// if err != nil {
+	// 	log.Printf("Error: %v", err)
+	// 	return
+	// }
+
+	// err = json.Unmarshal(bytes, &user)
+	// if err != nil {
+	// 	log.Printf("Error: %v", err.Error())
+	// 	return
+	// }
+	if err := c.ShouldBindJSON(&user); err != nil {
+		restErr := errors.NewBadRequestError(err.Error())
+
+		c.JSON(restErr.Status, restErr)
+		return
+	}
+
+	result, err := services.CreateUser(user)
+	if err != nil {
+		c.JSON(err.Status, err)
+		return
+	}
+
+	c.JSON(http.StatusCreated, result)
 }
 
 func GetUser(c *gin.Context) {
--- services/user_service.go
+++ services/user_service.go
@@ -1,10 +1,15 @@
 package services
 
-func GetUser() {
+import (
+	"github.com/edilbertloquine/go-microservices/users-api/domain/users"
+	"github.com/edilbertloquine/go-microservices/users-api/utils/errors"
+)
 
+func CreateUser(user users.User) (*users.User, *errors.RestErr) {
+	return &user, nil
 }
 
-func CreateUser() {
+func GetUser() {
 
 }
 
