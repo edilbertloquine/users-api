30e7659a9a4b7f1fc38c11a86235deca48b0a443
Edilbert Loquine <edilbert@whitewidget.com>
2020-05-27 22:09:11 +0800

    feat(project): Setup data persistence structure and method stubs

--- controllers/users/users_controller.go
+++ controllers/users/users_controller.go
@@ -2,6 +2,7 @@ package users
 
 import (
 	"net/http"
+	"strconv"
 
 	"github.com/edilbertloquine/go-microservices/users-api/domain/users"
 	"github.com/edilbertloquine/go-microservices/users-api/services"
@@ -12,18 +13,6 @@ import (
 
 func CreateUser(c *gin.Context) {
 	var user users.User
-
-	// bytes, err := ioutil.ReadAll(c.Request.Body)
-	// if err != nil {
-	// 	log.Printf("Error: %v", err)
-	// 	return
-	// }
-
-	// err = json.Unmarshal(bytes, &user)
-	// if err != nil {
-	// 	log.Printf("Error: %v", err.Error())
-	// 	return
-	// }
 	if err := c.ShouldBindJSON(&user); err != nil {
 		restErr := errors.NewBadRequestError(err.Error())
 
@@ -41,7 +30,20 @@ func CreateUser(c *gin.Context) {
 }
 
 func GetUser(c *gin.Context) {
-	c.String(http.StatusNotImplemented, "Implement this")
+	userId, err := strconv.ParseInt(c.Param("user_id"), 10, 64)
+	if err != nil {
+		err := errors.NewBadRequestError("invalid user id")
+		c.JSON(err.Status, err)
+		return
+	}
+
+	user, errr := services.GetUser(userId)
+	if err != nil {
+		c.JSON(errr.Status, errr)
+		return
+	}
+
+	c.JSON(http.StatusOK, user)
 }
 
 func SearchUser(c *gin.Context) {
--- services/user_service.go
+++ services/user_service.go
@@ -5,12 +5,25 @@ import (
 	"github.com/edilbertloquine/go-microservices/users-api/utils/errors"
 )
 
-func CreateUser(user users.User) (*users.User, *errors.RestErr) {
-	return &user, nil
+func GetUser(userId int64) (*users.User, *errors.RestErr) {
+	result := &users.User{Id: userId}
+	if err := result.Get(); err != nil {
+		return nil, err
+	}
+
+	return result, nil
 }
 
-func GetUser() {
+func CreateUser(user users.User) (*users.User, *errors.RestErr) {
+	if err := user.Validate(); err != nil {
+		return nil, err
+	}
 
+	if err := user.Save(); err != nil {
+		return nil, err
+	}
+
+	return &user, nil
 }
 
 func FindUser() {
